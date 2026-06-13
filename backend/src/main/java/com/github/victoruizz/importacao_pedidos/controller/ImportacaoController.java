ae769475f8200c43334468415e5690556a17bcc3
Victor Ruiz <ruizvictor077@outlook.com>
2026-06-13 18:09:16 -0300

    feat(backend): endpoint de listagem de importacoes

--- backend/src/main/java/com/github/victoruizz/importacao_pedidos/controller/ImportacaoController.java
+++ backend/src/main/java/com/github/victoruizz/importacao_pedidos/controller/ImportacaoController.java
@@ -1,13 +1,13 @@
 package com.github.victoruizz.importacao_pedidos.controller;
 
+import com.github.victoruizz.importacao_pedidos.entity.Importacao;
 import com.github.victoruizz.importacao_pedidos.service.ImportacaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/importacoes")
@@ -20,5 +20,12 @@ public class ImportacaoController {
     public ResponseEntity<Long> criar(@RequestParam("arquivo") MultipartFile arquivo) {
         Long id = importacaoService.criarImportacao(arquivo);
         return ResponseEntity.status(201).body(id);
-}
+    }
+
+    @GetMapping
+    public List<Importacao> listar() {
+        return importacaoService.listarImportacoes();
+    }
+
+
 }
--- backend/src/main/java/com/github/victoruizz/importacao_pedidos/service/ImportacaoService.java
+++ backend/src/main/java/com/github/victoruizz/importacao_pedidos/service/ImportacaoService.java
@@ -14,6 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -45,6 +46,10 @@ public class ImportacaoService {
         rabbitTemplate.convertAndSend(RabbitConfig.FILA_IMPORTACAO, salva.getId());
 
         return salva.getId();
+    }
+
+    public List<Importacao> listarImportacoes() {
+        return importacaoRepository.findAll();
     }
 
 }
