fe6a0473b06f0575a5083574ffc5edb1c853892d
Victor Ruiz <ruizvictor077@outlook.com>
2026-06-13 13:57:38 -0300

    feat(backend): publica id da importacao na fila do rabbitmq

--- backend/src/main/java/com/github/victoruizz/importacao_pedidos/service/ImportacaoService.java
+++ backend/src/main/java/com/github/victoruizz/importacao_pedidos/service/ImportacaoService.java
@@ -1,5 +1,6 @@
 package com.github.victoruizz.importacao_pedidos.service;
 
+import com.github.victoruizz.importacao_pedidos.config.RabbitConfig;
 import com.github.victoruizz.importacao_pedidos.entity.Importacao;
 import com.github.victoruizz.importacao_pedidos.entity.StatusImportacao;
 import com.github.victoruizz.importacao_pedidos.repository.ImportacaoRepository;
@@ -26,6 +27,8 @@ public class ImportacaoService {
         importacao.setLinhasInvalidas(0);
 
         Importacao salva = importacaoRepository.save(importacao);
+
+        rabbitTemplate.convertAndSend(RabbitConfig.FILA_IMPORTACAO, salva.getId());
 
         return salva.getId();
     }
